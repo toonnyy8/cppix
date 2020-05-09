38c8c7d2aa5e1b8440e15659b1ddcf6531c50956
toonnyy8 <toonnyy8@gmail.com>
2020-05-10 00:15:29 +0800

    init

--- main.cpp
+++ main.cpp
@@ -1,37 +1,23 @@
+#include <ctype.h>
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 
-// Regular background
-#define BLKB "\e[40m"
-#define REDB "\e[41m"
-#define GRNB "\e[42m"
-#define YELB "\e[43m"
-#define BLUB "\e[44m"
-#define MAGB "\e[45m"
-#define CYNB "\e[46m"
-#define WHTB "\e[47m"
+std::string color(int r, int g, int b)
+{
+  return "\x1b[48;" + std::to_string(r) + ";" + std::to_string(g) + ";" + std::to_string(b) + ";0m\u3000\x1b[0m";
+}
 
-// High intensty background
-#define BLKHB "\e[0;100m"
-#define REDHB "\e[0;101m"
-#define GRNHB "\e[0;102m"
-#define YELHB "\e[0;103m"
-#define BLUHB "\e[0;104m"
-#define MAGHB "\e[0;105m"
-#define CYNHB "\e[0;106m"
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   struct winsize w;
   ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 
   printf("lines %d\n", w.ws_row);
   printf("columns %d\n", w.ws_col);
 
-  std::cout << YELHB << "\u3000" << BLKB << std::endl;
-  std::cout << YELB << "\u3000" << BLKB << std::endl;
-  std::cout << BLKB << "\u3000";
+  std::cout << color(2, 255, 100) << std::endl;
   return 0; // make sure your main returns int
 }
