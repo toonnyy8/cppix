39289b4ccf33f02e78a2f43ff8001de979300dc8
toonnyy8 <toonnyy8@gmail.com>
2020-05-09 12:54:21 +0800

    init

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
+
+// Regular background
+#define BLKB "\e[40m"
+#define REDB "\e[41m"
+#define GRNB "\e[42m"
+#define YELB "\e[43m"
+#define BLUB "\e[44m"
+#define MAGB "\e[45m"
+#define CYNB "\e[46m"
+#define WHTB "\e[47m"
+
+// High intensty background
+#define BLKHB "\e[0;100m"
+#define REDHB "\e[0;101m"
+#define GRNHB "\e[0;102m"
+#define YELHB "\e[0;103m"
+#define BLUHB "\e[0;104m"
+#define MAGHB "\e[0;105m"
+#define CYNHB "\e[0;106m"
+#define WHTHB "\e[0;107"
 
 #define KNRM "\x1B[0m"
 #define KRED "\x1B[31m"
@@ -30,6 +51,8 @@ int main(int argc, char **argv) {
 
   printf("\033[031m bgColor is now red \033[047m And now white.\n");
 
-  printf("\u3000");
+  std::cout << ""
+            << "\u3000";
+  std::cout << YELB << "\u3000";
   return 0; // make sure your main returns int
 }
