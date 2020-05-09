1c821678633f04d4a3892b27c2c98dac30b84364
toonnyy8 <toonnyy8@gmail.com>
2020-05-09 22:50:59 +0800

    init

--- main.cpp
+++ main.cpp
@@ -22,16 +22,6 @@
 #define BLUHB "\e[0;104m"
 #define MAGHB "\e[0;105m"
 #define CYNHB "\e[0;106m"
-#define WHTHB "\e[0;107m"
-
-#define KNRM "\x1B[0m"
-#define KRED "\x1B[31m"
-#define KGRN "\x1B[32m"
-#define KYEL "\x1B[33m"
-#define KBLU "\x1B[34m"
-#define KMAG "\x1B[35m"
-#define KCYN "\x1B[36m"
-#define KWHT "\x1B[37m"
 
 int main(int argc, char **argv) {
   struct winsize w;
@@ -39,15 +29,6 @@ int main(int argc, char **argv) {
 
   printf("lines %d\n", w.ws_row);
   printf("columns %d\n", w.ws_col);
-
-  printf("%sred\n", KRED);
-  printf("%sgreen\n", KGRN);
-  printf("%syellow\n", KYEL);
-  printf("%sblue\n", KBLU);
-  printf("%smagenta\n", KMAG);
-  printf("%scyan\n", KCYN);
-  printf("%swhite\n", KWHT);
-  printf("%s \n", KNRM);
 
   printf("\033[031m bgColor is now red \033[047m And now white.\n");
 
