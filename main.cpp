0e35828cd37d448f5f91e2b08340387ea619b447
toonnyy8 <toonnyy8@gmail.com>
2020-05-10 13:45:50 +0800

    init

--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
-#include <ctype.h>
+#include <cstdio>
+#include <cstdlib>
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
+#include <memory>
 #include <sys/ioctl.h>
 #include <unistd.h>
+#include <vector>
 
-std::string color(std::string r, std::string g, std::string b)
-{
+std::string color(std::string r, std::string g, std::string b) {
   return "\x1b[48;2;" + r + ";" + g + ";" + b + "m\u3000\x1b[0m";
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   struct winsize w;
   ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 
