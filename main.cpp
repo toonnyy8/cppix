960bc3c7a90a0bb445187c2d05721f6ae4e16471
toonnyy8 <toonnyy8@gmail.com>
2020-05-17 15:09:23 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,19 +1,19 @@
-#include <cstdio>
-#include <cstdlib>
+#include <chrono>
 #include <iostream>
 #include <memory>
-#include <sys/ioctl.h>
-#include <unistd.h>
+#include <thread>
 #include <vector>
 
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 
-struct RGB {
+struct RGB
+{
   char r;
   char g;
   char b;
 };
-std::string decode_color(char color_code) {
+std::string decode_color(char color_code)
+{
   std::string color_str;
   color_str = "";
   int cc;
@@ -27,18 +27,14 @@ std::string decode_color(char color_code) {
 
   return color_str;
 }
-std::string color(struct RGB rgb) {
+std::string color(struct RGB rgb)
+{
   return "\x1b[48;2;" + decode_color(rgb.r) + ";" + decode_color(rgb.g) + ";" +
          decode_color(rgb.b) + "m\u3000\x1b[0m";
 }
 
-int main(int argc, char **argv) {
-  struct winsize w;
-  ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-
-  printf("lines %d\n", w.ws_row);
-  printf("columns %d\n", w.ws_col);
-
+int main(int argc, char **argv)
+{
   const int H = 10;
   const int W = 10;
   const int VH = 10;
@@ -47,24 +43,41 @@ int main(int argc, char **argv) {
   int offset_x;
   offset_y = 0;
   offset_x = 0;
+  int frame_num;
+  frame_num = 10;
+  int fps;
+  fps = 10;
 
-  std::vector<std::vector<struct RGB>> map;
+  std::vector<std::vector<std::vector<struct RGB>>>
+      pixel_maps;
 
-  for (int y = 0; y < H; y++) {
-    map.push_back(std::vector<struct RGB>());
-    for (int x = 0; x < W; x++) {
-      map[y].push_back({(char)(x + y), (char)(10 * x), (char)(10 * y)});
+  for (int t = 0; t < frame_num; t++)
+  {
+    pixel_maps.push_back(std::vector<std::vector<struct RGB>>());
+    for (int y = 0; y < H; y++)
+    {
+      pixel_maps[t].push_back(std::vector<struct RGB>());
+      for (int x = 0; x < W; x++)
+      {
+        pixel_maps[t][y].push_back({(char)(x + y), (char)(10 * x), (char)(10 * y)});
+      }
     }
   }
 
-  for (int y = offset_y; y < MIN(offset_y + VH, H); y++) {
-    for (int x = offset_x; x < MIN(offset_x + VW, W); x++) {
-      std::cout << color(map[y][x]);
+  for (int t = 0; t < frame_num; t++)
+  {
+    for (int y = offset_y; y < MIN(offset_y + VH, H); y++)
+    {
+      for (int x = offset_x; x < MIN(offset_x + VW, W); x++)
+      {
+        std::cout << color(pixel_maps[t][y][x]);
+      }
+      std::cout << std::endl;
     }
-    std::cout << std::endl;
+    std::chrono::duration<int, std::milli> timespan(1000);
+    std::this_thread::sleep_for(timespan);
+    // std::cout << "\033[0;0H\033[2J";
   }
-
-  //  std::cout << "\033[0;0H\033[2J";
 
   return 0;
 }
